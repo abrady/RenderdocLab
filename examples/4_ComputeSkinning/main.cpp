39327e7e6bcb950d8bbbe5c668dfc7f23cf6e1f8
Aaron Brady <aaronabrady@yahoo.com>
2025-06-14 16:27:48 -0700

    fix backface culling.

--- examples/4_ComputeSkinning/main.cpp
+++ examples/4_ComputeSkinning/main.cpp
@@ -261,8 +261,8 @@ protected:
         rasterizer.rasterizerDiscardEnable = VK_FALSE;
         rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
         rasterizer.lineWidth = 1.0f;
-        rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-        rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+        rasterizer.cullMode = VK_CULL_MODE_NONE; // the cylinder isn't closed so draw both sides
+        rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
         rasterizer.depthBiasEnable = VK_FALSE;
 
         VkPipelineMultisampleStateCreateInfo multisampling{};
